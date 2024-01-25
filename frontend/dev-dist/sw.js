b97a705dad2dd4a313c85369faaa9d0011874745
Gleb Orlovtsev <the.unknown.guest@yandex.com>
2024-01-25 13:35:53 +0300

    manifest config moved to a different folder

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.n8jtha9c5s8"
+    "revision": "0.24tc0gutl5o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,50 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
-
-const manifestForPlugin: Partial<VitePWAOptions> = {
-registerType:'prompt',
-includeAssets:['favicon.ico','apple-touch-icon','mask-icon.svg'],
-srcDir:'src',
-devOptions: {
-  enabled:true
-},
-manifest: {
-  name: 'Shop.com',
-  short_name: 'Shop',
-  description: 'Shop for games.',
-  theme_color:'#171717',
-  display:'standalone',
-  scope:'/',
-  start_url:'/',
-  orientation:'portrait',
-  icons: [
-    {
-      src:'pwa-64x64.png',
-      sizes:'64x64',
-      type:'image/png'
-    },
-    {
-      src:'pwa-192x192.png',
-      sizes:'192x192',
-      type:'image/png'
-    },
-    {
-      src:'pwa-512x512.png',
-      sizes:'512x512',
-      type:'image/png',
-      purpose: 'any'
-    },
-    {
-      src:'maskable-icon-512x512.png',
-      sizes:'512x512',
-      type:'image/png',
-      purpose:'maskable'
-    }
-  ]
-}
-
-}
+import { VitePWA } from "vite-plugin-pwa";
+import { manifestForPlugin } from './manifestConfig';
 
 // https://vitejs.dev/config/
 export default defineConfig({
